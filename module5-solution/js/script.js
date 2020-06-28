cc305563beef120875733fdf6c9dca02f39112d4
Soham Moghe <sohammoghe202001@gmail.com>
2020-06-28 23:21:24 +0530

    Typo fixed

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
       
-      var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
+      var chosenCategoryShortName = chooseRandomCategory (categories);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
